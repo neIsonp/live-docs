59b5d96035a7078000db38b82344ce4b03e06c7d
neIsonp <nelsonponte37@gmail.com>
2024-07-27 10:45:31 +0000

    realtime features

---
+++ app/Provider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Loader from "@/components/Loader";
+import {
+  ClientSideSuspense,
+  LiveblocksProvider,
+} from "@liveblocks/react/suspense";
+
+import React, { ReactNode } from "react";
+
+const Provider = ({ children }: { children: ReactNode }) => {
+  return (
+    <LiveblocksProvider authEndpoint="/api/liveblock-auth">
+      <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
+    </LiveblocksProvider>
+  );
+};
+
+export default Provider;
---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import React, { ReactNode } from "react";
+import Header from "./Header";
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
+import { Editor } from "./editor/Editor";
+
+const CollaborativeRoom = () => {
+  return (
+    <RoomProvider id="my-room">
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">How to be a millionaire</p>
+            </div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
